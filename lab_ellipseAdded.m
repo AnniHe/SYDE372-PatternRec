6dcf6975c62d78cfed91426621638ddcf0e33b73
raphaelkalker <raphaelkalker@users.noreply.github.com>
2015-02-06 01:30:23 -0500

    added plot nn and nn classifier

--- lab_ellipseAdded.m
+++ lab_ellipseAdded.m
@@ -260,3 +260,49 @@ function classifier = plot_GED(grid, xVals, yVals, mean_transformed_Args, mat_tr
         classifier(i) = classIndex;       
     end
 end
+
+function plot_nn(k, grid, xVals, yVals, fig, dataA, dataB)
+
+    classifier = repmat(grid, 1);
+    
+    for i = 1:numel(classifier)
+        [row col] = ind2sub(size(classifier), i);
+        classIndex = NN_Class(k,[xVals(col);yVals(row)], fig, dataA, dataB);
+        classifier(i) = classIndex; 
+    end
+    
+    figure(1);
+    contour(xVals,yVals,classifier);
+
+end
+
+function classType = NN_Class(k, point, fig, varargin)
+
+    minDistances = [];
+    %for A,B,C,D,E
+    for i = 1:length(varargin)
+        classDistances = [];
+        %each row
+        for j = 1:length(varargin{i})
+            %[row col] = ind2sub(size(varargin{i}), j);
+            distance = sqrt((varargin{i}(j,1) - point(1))^2 + (varargin{i}(j,2) - point(2)).^2);
+            classDistances =[classDistances distance];
+        end
+        
+        if k > 1
+            tmp = sort(classDistances);
+            sorted = tmp(1:k);
+            avgDistance = mean(sorted);
+            minDistances = [minDistances avgDistance];
+        else
+            closest = min(classDistances);
+            minDistances = [minDistances closest];
+            
+        end
+            
+    end
+    %classType index should be 1 or 2
+    [dist, classType] = min(minDistances);
+
+    
+end
